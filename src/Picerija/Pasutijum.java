a3ec906447aa9a78b54db8c8ec381e656f0ef001
Makarovs <rolandsmakarovs264@gmail.com>
2023-05-25 17:30:43 +0300

    Viean Faila Izlsīšana

--- src/Picerija/Pasutijum.java
+++ src/Picerija/Pasutijum.java
@@ -28,6 +28,107 @@ switch(izvele) {
 
 case "Pasūtīttie Pasūtījumi":
 
+	
+	String desktopPath = System.getProperty("user.home") + "\\Desktop\\"; //C:\Users\2PT-2\Desktop\Saņemtie_Pasūtījumi
+
+	String Fails = desktopPath + "Pasūtītās_Picas";
+
+	
+
+	File FailuVieta = new File(Fails);
+
+	
+
+	if (!FailuVieta.exists() || !FailuVieta.isDirectory()) {
+
+	JOptionPane.showMessageDialog(null, "Nav Atrasts: " + Fails,
+
+	"Error", JOptionPane.ERROR_MESSAGE);
+
+	return;
+
+	}
+
+	
+
+	File[] files = FailuVieta.listFiles();
+
+	if (files.length == 0) {
+
+	JOptionPane.showMessageDialog(null, "Nav Psutijumi: ",
+
+	"Tukss", JOptionPane.ERROR_MESSAGE);
+
+	return;
+
+	}
+
+	
+
+	String[] fileNos = new String[files.length];
+
+	for (int i = 0; i < files.length; i++) {
+
+		fileNos[i] = files[i].getName();
+
+	}
+
+	String FailuVards = (String) JOptionPane.showInputDialog(null, "Choose a file to open:",
+
+	"Open File", JOptionPane.PLAIN_MESSAGE, null, fileNos, fileNos[0]);
+
+	if (FailuVards == null) {
+
+	return; 
+
+	}
+
+	
+
+	for (File file : files) {
+
+	if (file.getName().equals(FailuVards)) {
+
+	try {
+
+	FileReader reader = new FileReader(file);
+
+	BufferedReader bufferedReader = new BufferedReader(reader);
+
+	StringBuilder contentBuilder = new StringBuilder();
+
+	String line;
+
+	while ((line = bufferedReader.readLine()) != null) {
+
+	contentBuilder.append(line);
+
+	contentBuilder.append(System.lineSeparator());
+
+	}
+
+	bufferedReader.close();
+
+	
+
+	JOptionPane.showMessageDialog(null, contentBuilder.toString(),
+
+	"File Contents: " + file.getName(), JOptionPane.INFORMATION_MESSAGE);
+
+	} catch (IOException e) {
+
+	
+
+	JOptionPane.showMessageDialog(null, "Error reading file: " + e.getMessage(),
+
+	"File Read Error", JOptionPane.ERROR_MESSAGE);
+
+	}
+
+	break;
+
+	}	
+}
 break;
 
 case "Klātienes Pasūtījumi":
